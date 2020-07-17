a5808cdf48efdb0c43722c391efa9101e948fd67
Othman Mohammed <uniqueginun@gmail.com>
2020-07-17 23:58:55 +0300

    second commit

--- resources/views/admin-panel/_inc/footer.blade.php
+++ resources/views/admin-panel/_inc/footer.blade.php
@@ -3,6 +3,6 @@
     </strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-        Makkah Portal, <b>Version</b> 2.0
+        Admin Panel, <b>Version</b> 2.0
     </div>
 </footer>
